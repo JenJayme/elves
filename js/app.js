00f286ddf22ebed5a331f12c2c897ec81b114ab9
Jen Jayme <jeniferjayme@gmail.com>
2020-11-30 01:17:24 -0800

    Added contact form, emailjs service, and optimized videos

--- js/app.js
+++ js/app.js
@@ -1,7 +1,40 @@
 $(document).foundation()
 
+function getInputs() {
+    'use strict';
+    var input, firstName, lastName, petName, petType, phone, email, date, time;
+  
+      // alert('Submit button clicked');
+      firstName = $('#firstName').val();
+      lastName = $('#lastName').val();
+      phone = $('#phone').val();
+      email = $('#email').val();
+      address = $('#address').val();
+      city = $('#city').val();
+      giftQty = $('#giftQty').val();
+      trips = $('#trips').val();
+      input = {
+        'firstName': firstName,
+        'lastName': lastName,
+        'phone': phone,
+        'email': email,
+        'address': address,
+        'city': city,
+        'giftQty': giftQty,
+        'trips': trips
+      }
+      console.log("Input:", input)
+      return input;
+  }
 
 $( document ).ready(function() {
     AOS.init();
     console.log( "Ready!" );
+
+    reqForm.on('submit', function (event) {
+        event.preventDefault();
+        var input = getInputs();
+        emailjs.sendForm('contact_service', 'elfForm', this);
+      })
+  
 });
